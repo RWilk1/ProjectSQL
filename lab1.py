a2410749a8f1b9af18c48f1aca7de37301bf97fb
rawilk <rwilk1@outlook.com>
2023-06-10 09:25:09 +0200

    First Connection String

--- lab1.py
+++ lab1.py
@@ -1,1 +1,24 @@
 print("Hi")
+
+# Laczenie sie z bazami danych
+
+from dotenv import load_dotenv
+import os
+import pyodbc
+
+load_dotenv()
+database_password = os.environ.get("DATABASE_PASSWORD")
+print(database_password)
+database_server = 'morfeusz.wszib.edu.pl'
+driver = "ODBC Driver 18 for SQL Server"
+database_user = 'rawilk'
+database_name = 'rawilk'
+
+# 1) Utworzenie connection-string-a
+connection_string = f'Driver={driver};' \
+                    f'SERVER={database_server};' \
+                    f'DATABASE={database_name};' \
+                    f'UID={database_user};'\
+                    f'PWD={database_password};' \
+                    'Encrypt=no;'
+connection = pyodbc.connect(connection_string)
